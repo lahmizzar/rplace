acc60abeb4c48df69e3153edf291a5875b7b9b37
Lahmizzar Valeryon <lahmizzar@devxive.com>
2022-04-04 12:05:13 +0200

    initial commit

---
+++ template_userscript.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         placede manual templates
+// @namespace    https://github.com/lahmizzar/rplace
+// @version      1.0
+// @description  simple overlay system for r/place based on tt2468's script
+// @author       Lahmizzar
+// @match        https://hot-potato.reddit.com/embed*
+// @match        https://www.reddit.com/r/place/*
+// @match        https://new.reddit.com/r/place/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/lahmizzar/rplace/raw/main/userscript.js
+// @downloadURL  https://github.com/lahmizzar/rplace/raw/main/userscript.js
+// @grant        none
+// @license      GNU GPLv3
+// ==/UserScript==
+
+
+
+
+
+var child = null;
+
+function getImage() {
+    const i = document.createElement("img");
+    i.src = "https://github.com/lahmizzar/rplace/raw/main/place.png?t=" + Date.now();
+    i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+    return i;
+}
+
+function refreshTemplate() {
+    var x = document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0];
+    if (child) {
+        x.removeChild(child);
+    }
+    child = getImage();
+    x.appendChild(child, false);
+
+    console.log("Template has been updated.");
+}
+
+function refreshTemplateLoop() {
+    refreshTemplate();
+    setTimeout(function() { refreshTemplateLoop(); }, 300*1000);
+}
+
+(function() {
+    function addButton(text, onclick, cssObj) {
+        cssObj = cssObj || {position: 'absolute', bottom: '5%', left:'4%', 'z-index': 3}
+        let button = document.createElement('button'), btnStyle = button.style
+        document.body.appendChild(button)
+        button.innerHTML = text
+        button.onclick = onclick
+        btnStyle.position = 'absolute'
+        Object.keys(cssObj).forEach(key => {btnStyle[key] = cssObj[key]})
+        return button
+    }
+
+    window.addEventListener('load', () => {
+        setTimeout(function() { refreshTemplateLoop(); }, 4000);
+        addButton('Update Template', refreshTemplate)
+    })
+})();
